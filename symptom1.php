b09bc409c38fc2b1584889c4e133c56b1c74e788
Hyorin Kim <hyoriinvic@gmail.com>
2020-11-12 22:13:24 +0900

    update

--- symptom1.php
+++ symptom1.php
@@ -24,14 +24,12 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
     <!-- js -->
-    <script type="text/javascript" src="./js/selectedSymptom.js"></script> <!-- check 되면 value 바꿔주는 isChecked() -->
-    <!-- <script type="text/javascript" src="./js/postSymptom1.js"></script> -->
+    <!-- <script type="text/javascript" src="./js/selectedSymptom.js"></script> check 되면 value 바꿔주는 isChecked() 
+    <script type="text/javascript" src="./js/postSymptom1.js"></script>--> 
     <script type="text/javascript"> 
         // 제출하기 버튼을 누를 경우,          
         $(document).ready(function(){
             $('submitInfo').click(function(){
-                isChecked();
-
                 var action = $('submitInfo').attr('action');
                 var form_data = {
                     high_blood_pressure : $("$high_blood_pressure").val(),
@@ -68,35 +66,35 @@
     <form action = './php/symptomInput1.php' method = "POST">
         <div class="checkbox-container">
             <div>
-                <input type="checkbox" name = 'high_blood_pressure' class="btns" id="high_blood_pressure" value = '0'>
+                <input type="checkbox" name = 'high_blood_pressure' class="btns" id="high_blood_pressure" value='1'>
                 <label class="btn1" for="high_blood_pressure">고혈압</label>
             </div>
             <div>
-                <input type="checkbox" name = 'diabetes' class="btns" id="diabetes" value = '0'>
+                <input type="checkbox" name = 'diabetes' class="btns" id="diabetes" value='1'>
                 <label class="btn1" for="diabetes">당뇨</label>
             </div>
             <div>
-                <input type="checkbox" name = 'hepatitis' class="btns" id="hepatitis" value = '0'>
+                <input type="checkbox" name = 'hepatitis' class="btns" id="hepatitis" value='1'>
                 <label class="btn1" for="hepatitis">간염</label>
             </div>
             <div>
-                <input type="checkbox" name = 'allergy' class="btns" id="allergy" value = '0'>
+                <input type="checkbox" name = 'allergy' class="btns" id="allergy" value='1'>
                 <label class="btn1" for="allergy">알레르기</label>
             </div>
             <div>
-                <input type="checkbox" name = 'surgery_history' class="btns" id="surgery_history" value = '0'>
+                <input type="checkbox" name = 'surgery_history' class="btns" id="surgery_history" value='1'>
                 <label class="btn1" for="surgery_history">수술 이력</label>
             </div>
             <div>
-                <input type="checkbox" name = 'medication' class="btns" id="medication" value = '0'>
+                <input type="checkbox" name = 'medication' class="btns" id="medication" value='1'>
                 <label class="btn1" for="medication">복용약</label>
             </div>
             <div>
-                <input type="checkbox" name = 'drinking' class="btns" id="drinking" value = '0'>
+                <input type="checkbox" name = 'drinking' class="btns" id="drinking" value='1'>
                 <label class="btn2" for="drinking">음주</label>
             </div>
             <div>
-                <input type="checkbox" name = 'smoking' class="btns" id="smoking" value = '0'>
+                <input type="checkbox" name = 'smoking' class="btns" id="smoking" value='1'>
                 <label class="btn2" for="smoking">흡연</label>
             </div>
         </div>
