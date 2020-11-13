5cd1fa1e5e1858f9356d69e0467be2e3e7ffb084
Hyorin Kim <hyoriinvic@gmail.com>
2020-11-13 12:50:44 +0900

    update

--- symptom1-1.php
+++ symptom1-1.php
@@ -6,19 +6,6 @@
     <link rel="stylesheet" href="css/background.css" />
     <link rel="stylesheet" href="css/symptom1/symptom1.css" />
 
-    <?php
-    session_cache_expire(5); //세션이 유지될 시간을 입력합니다.
-    session_start();
-
-    $join_patientName	= $_SESSION['join_patientName'];   //세션에서 값을 받아옵니다.
-    $join_patientTel = $_SESSION['join_patientTel']; //세션에서 값을 받아옵니다.
-
-    if(!$join_patientName) {
-        header('location:/main.php');
-      //아이디값이 없을경우 세션이 만료되었다는 의미이므로 처리할 코드를 입력합니다.
-    }	
-    ?>
-
     <!-- jquery cdn -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
@@ -35,10 +22,11 @@
             $.each(btns, function (index, item) {
                 if ($(item).is(":checked") == true) {
                     $(item).val("1");
-                    //checked.push("1");
+                }else{
+                    $(item).val("0");
                 }
-                console.log($(item).val());
                 checked.push($(item).val());            
+                console.log($(item).val());
             })
             console.log(checked);
             return checked;
@@ -51,32 +39,33 @@
                 var data = isChecked();
                 console.log(data);
                 console.log(data.length);
+                console.log(typeof(data[0]));
 
-                var action = $('submitInfo').attr('action');
-                var form_data = {
-                    high_blood_pressure : data[0],
-                    diabetes : data[1],
-                    hepatitis : data[2],
-                    allergy : data[3],
-                    surgery_history : data[4],
-                    medication : data[5],
-                    drinking : data[6]
-                    smoking : data[7]
-                };
+        //         var action = $('submitInfo').attr('action');
+        //         var form_data = {
+        //             high_blood_pressure : data[0],
+        //             diabetes : data[1],
+        //             hepatitis : data[2],
+        //             allergy : data[3],
+        //             surgery_history : data[4],
+        //             medication : data[5],
+        //             drinking : data[6],
+        //             smoking : data[7]
+        //         };
                     
-        $.ajax({
-            type:'POST',
-            url:action,
-            data:form_data,
-            success:function(response){
-                if(response.trim() == 'success'){
-                    $('msg').html('<p>증상입력 성공!</p>')
-                }else{
-                    $('msg').html('<p>증상입력 실패!</p>')
-                }
-            },
-            error:function(){ $('msg').html('<h2>error</h2>');}
-            });
+        // $.ajax({
+        //     type:'POST',
+        //     url:action,
+        //     data:form_data,
+        //     success:function(response){
+        //         if(response.trim() == 'success'){
+        //             $('msg').html('<p>증상입력 성공!</p>')
+        //         }else{
+        //             $('msg').html('<p>증상입력 실패!</p>')
+        //         }
+        //     },
+        //     error:function(){ $('msg').html('<h2>error</h2>');}
+        //     });
         });
     });
     </script>
@@ -88,35 +77,35 @@
     <form action = './php/symptomInput1.php' method = "POST">
         <div class="checkbox-container">
             <div>
-                <input type="checkbox" name = 'high_blood_pressure' class="btns" id="high_blood_pressure" value='0'>
+                <input type="checkbox" name = 'high_blood_pressure' class="btns" id="high_blood_pressure" value="0">
                 <label class="btn1" for="high_blood_pressure">고혈압</label>
             </div>
             <div>
-                <input type="checkbox" name = 'diabetes' class="btns" id="diabetes" value='0'>
+                <input type="checkbox" name = 'diabetes' class="btns" id="diabetes" value="0">
                 <label class="btn1" for="diabetes">당뇨</label>
             </div>
             <div>
-                <input type="checkbox" name = 'hepatitis' class="btns" id="hepatitis" value='0'>
+                <input type="checkbox" name = 'hepatitis' class="btns" id="hepatitis" value="0">
                 <label class="btn1" for="hepatitis">간염</label>
             </div>
             <div>
-                <input type="checkbox" name = 'allergy' class="btns" id="allergy" value='0'>
+                <input type="checkbox" name = 'allergy' class="btns" id="allergy" value="0">
                 <label class="btn1" for="allergy">알레르기</label>
             </div>
             <div>
-                <input type="checkbox" name = 'surgery_history' class="btns" id="surgery_history" value='0'>
+                <input type="checkbox" name = 'surgery_history' class="btns" id="surgery_history" value="0">
                 <label class="btn1" for="surgery_history">수술 이력</label>
             </div>
             <div>
-                <input type="checkbox" name = 'medication' class="btns" id="medication" value='0'>
+                <input type="checkbox" name = 'medication' class="btns" id="medication" value="0">
                 <label class="btn1" for="medication">복용약</label>
             </div>
             <div>
-                <input type="checkbox" name = 'drinking' class="btns" id="drinking" value='0'>
+                <input type="checkbox" name = 'drinking' class="btns" id="drinking" value="0">
                 <label class="btn2" for="drinking">음주</label>
             </div>
             <div>
-                <input type="checkbox" name = 'smoking' class="btns" id="smoking" value='0'>
+                <input type="checkbox" name = 'smoking' class="btns" id="smoking" value="0">
                 <label class="btn2" for="smoking">흡연</label>
             </div>
         </div>
