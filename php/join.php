59f5264ea60aff2b9a45aad878721177a8afe7da
MinsuKim3095 <68323158+MinsuKim3095@users.noreply.github.com>
2020-11-25 12:39:38 +0900

    201125A

--- php/join.php
+++ php/join.php
@@ -15,15 +15,17 @@
     $join_juminNumber = $_POST['join_juminNumber']; //주민등록번호
     $join_recommendPatient = $_POST['join_recommendPatient']; //추천자
 
+    echo '<pre>';print_r($_POST);echo '</pre>';
+
 //query 전송
-    $sql = 'insert into patient_db (join_patientName, join_patientTel, join_addressSearch, join_addressDetail, join_addressExtra, join_juminNumber, join_recommendPatient)';
+    $sql = 'INSERT INTO patient_db (join_patientName, join_patientTel, join_addressSearch, join_addressDetail, join_addressExtra, join_juminNumber, join_recommendPatient)';
     $sql = $sql."values('$join_patientName', '$join_patientTel', '$join_addressSearch', '$join_addressDetail', '$join_addressExtra', $join_juminNumber', '$join_recommendPatient')";
     if($connect->query($sql)){
         echo "<script>alert(\"회원가입이 완료되었습니다.\");</script>";
     }else{
         echo "<script>alert(\"회원가입에 실패했습니다. 관리자에게 문의하십시오.\");</script>";
     };
-    header('location:/main.php');
+    //header('location:/main.php');
 mysqli_close($connect);
 ?>
 
--- php/symptomInput1.php
+++ php/symptomInput1.php
@@ -4,27 +4,38 @@
     $pw = 'autoset';
     $dbName = 'kyunghee';
     $connect = new mysqli($host, $user, $pw, $dbName); 
+    session_start();
+ 
+    $high_blood_pressure = $_POST['high_blood_pressure']; //고혈압
+    $diabetes = $_POST['diabetes']; //당뇨
+    $hepatitis = $_POST['hepatitis']; //간염
+    $allergy = $_POST['allergy']; //알레르기
+    $surgery_history = $_POST['surgery_history']; //수술 이력
+    $medication = $_POST['medication']; //복용약
+    $drinking = $_POST['drinking']; //음주
+    $smoking = $_POST['smoking']; //흡연
     
-    $return_array = Array();
-    if(isset($_POST['high_blood_pressure']) && isset($_POST['diabetes']) && isset($_POST['hepatitis']) 
-    && isset($_POST['allergy']) && isset($_POST['surgery_history']) && isset($_POST['medication']) 
-    && isset($_POST['drinking']) && isset($_POST['smoking'])){
 
-        $sql = 'INSERT INTO patient_db (high_blood_pressure, diabetes, hepatitis, allergy, surgery_history, medication, drinking, smoking)';
-        $sql = $sql."values('$high_blood_pressure', '$diabetes', '$hepatitis', '$allergy', '$surgery_history', '$medication', '$drinking', '$smoking')";
-        $result = mysqli_query($connect, $sql);
+    $sql = "UPDATE patient_db SET 
+            high_blood_pressure = '$high_blood_pressure',
+            diabetes  = '$diabetes',
+            hepatitis = '$hepatitis',
+            allergy = '$allergy',
+            surgery_history = '$surgery_history',
+            medication = '$medication',
+            drinking = '$drinking',
+            smoking = '$smoking' 
+            WHERE join_patientName = $_SESSION[join_patientName] AND join_patientTel = $_SESSION[join_patientTel]";
 
-        $while($row = mysqli_fetch_array($result)) {
-            $row_array['high_blood_pressure'] = $row['high_blood_pressure'];
-            $row_array['diabetes'] = $row['diabetes'];
-            $row_array['hepatitis'] = $row['hepatitis'];
-            $row_array['allergy'] = $row['allergy'];
-            $row_array['surgery_history'] = $row['surgery_history'];
-            $row_array['medication'] = $row['medication'];
-            $row_array['drinking'] = $row['drinking'];
-            $row_array['smoking'] = $row['smoking'];
-            array_push($return_array, $row_array);
-        }
-        
-        echo json_encode($return_array, JSON_UNESCAPED_UNICODE);
+    //'INSERT INTO patient_db (high_blood_pressure, diabetes, hepatitis, allergy, surgery_history, medication, drinking, smoking)';
+    //$sql = $sql."values('$high_blood_pressure', '$diabetes', '$hepatitis', '$allergy', '$surgery_history', '$medication', '$drinking', '$smoking')";
+    
+    if(query($connect)->query($sql)){
+        echo "<script>alert(\"증상입력이 완료되었습니다.\");</script>";
+    } else{
+        echo "<script>alert(\"증상입력에 실패했습니다. 관리자에게 문의하십시오.\");</script>";
+    };
+    header('location:/symptom2.php');
+
+    mysqli_close($connect);
 ?>
